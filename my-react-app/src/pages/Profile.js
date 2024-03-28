11df7e3fd84e9f34e6761972d3bc211fa2905a4e
Rebecca Noelia Walker <rebecca.noelia.walker@gmail.com>
2024-03-28 13:58:24 +0000

    craig page

--- my-react-app/src/pages/Profile.js
+++ my-react-app/src/pages/Profile.js
@@ -5,17 +5,23 @@ function Profile() {
       <>
         <div className="name">Craig Cares</div>
 
-        <div className='profile+bio'>
+        <div className='profilebio'>
 
           <div className="profile-pic">
             <img src='profilepic.jpg' width ='60%' />
           </div>
           <div className="bio">
+          <div className="prompt">Name</div>
+            <div className="response">Craig Charles</div>
             <div className="prompt">Who caring for?</div>
             <div className="response">My mum</div>
             <div className="prompt">Caring alone?</div>
-            <div>Relationship with caree</div>
-            <div>More about me: just a very caring man</div>
+            <div className="response">Yes</div>
+            <div className="prompt">Relationship with caree</div>
+            <div className="response">Son</div>
+            <div className="prompt">More about me</div>
+            <div className="response">I'm a devoted carer for my wonderful mum, and while it's a fulfilling role, I understand that the journey can sometimes feel a bit lonely. That's why I'm here, reaching out to connect with others who share similar experiences. When I'm not busy taking care of my loved ones, you can find me enjoying the simple pleasures of life. Whether it's taking leisurely walks in nature, savoring a cup of tea, or diving into a good book, I find joy in the little things.</div>
+      
           </div>
 
         </div>
