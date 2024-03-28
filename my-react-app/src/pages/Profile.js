abf5c96c0706ba40e934406a5336004cdbb5b48c
Rebecca Noelia Walker <rebecca.noelia.walker@gmail.com>
2024-03-28 14:16:29 +0000

    amended craig

--- my-react-app/src/pages/Profile.js
+++ my-react-app/src/pages/Profile.js
@@ -7,6 +7,7 @@ function Profile() {
       <>
         <div className="name">Craig Cares</div>
 
+<div className='container'>
         <div className='profilebio'>
 
           <div className="profile-pic">
@@ -21,15 +22,21 @@ function Profile() {
             <div className="response">Yes</div>
             <div className="prompt">Relationship with caree</div>
             <div className="response">Son</div>
-            <div className="prompt">More about me</div>
-            <div className="response">I'm a devoted carer for my wonderful mum, and while it's a fulfilling role, I understand that the journey can sometimes feel a bit lonely. That's why I'm here, reaching out to connect with others who share similar experiences. When I'm not busy taking care of my loved ones, you can find me enjoying the simple pleasures of life. Whether it's taking leisurely walks in nature, savoring a cup of tea, or diving into a good book, I find joy in the little things.</div>
       
           </div>
-
         </div>
+        <div className='about-me'>
 
+          <div className="prompt">More about me</div>
+          <div className="response">I'm a devoted carer for my wonderful mum. That's why I'm here, reaching out to connect 
+          with others who share similar experiences. When I'm not busy taking care of my loved ones, you can find me enjoying the simple pleasures of life. Whether it's taking leisurely walks in nature, savoring a cup of tea, or diving into a good book, I find joy in the little things.</div>
+          <br />
+          <div className="prompt">Groups and Events</div>
+          <div className="response">Currently not attending any events</div>
+          
 
-
+          </div>
+          </div>
       <Nav/>
       </>
     );
