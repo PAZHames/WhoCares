503bccf1771440511604b28c5fedbb9e6b660f10
Rebecca Noelia Walker <rebecca.noelia.walker@gmail.com>
2024-03-28 13:31:26 +0000

    craig profile

--- my-react-app/src/pages/Profile.js
+++ my-react-app/src/pages/Profile.js
@@ -1,7 +1,26 @@
+import './Profile.css';
+
 function Profile() {
     return (
       <>
-        <div>PROFILE</div>
+        <div className="name">Craig Cares</div>
+
+        <div className='profile+bio'>
+
+          <div className="profile-pic">
+            <img src='profilepic.jpg' width ='60%' />
+          </div>
+          <div className="bio">
+            <div className="prompt">Who caring for?</div>
+            <div className="response">My mum</div>
+            <div className="prompt">Caring alone?</div>
+            <div>Relationship with caree</div>
+            <div>More about me: just a very caring man</div>
+          </div>
+
+        </div>
+
+
 
 
       </>
